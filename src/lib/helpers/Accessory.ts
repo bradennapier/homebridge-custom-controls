55dd381ecc7bc3b365de4679ecb1ec7492c09d45
Braden Napier <bradynapier+github@gmail.com>
2022-08-16 01:52:16 -0400

    feat: remove information char

--- src/lib/helpers/Accessory.ts
+++ src/lib/helpers/Accessory.ts
@@ -77,42 +77,41 @@ export class Accessory {
    */
   public setInformation(information: AccessoryInformation) {
     // Makes sure the accessory information service is used
-    const service = this.useService(
-      this.platform.Service.AccessoryInformation,
-      this.params.name,
-    );
-
-    // Updates the information characteristics
-    if (information.manufacturer) {
-      service.useCharacteristic(
-        this.platform.api.hap.Characteristic.Manufacturer,
-        information.manufacturer,
-      );
-    }
-    if (information.model) {
-      service.useCharacteristic(
-        this.platform.api.hap.Characteristic.Model,
-        information.model,
-      );
-    }
-    if (information.serialNumber) {
-      service.useCharacteristic(
-        this.platform.api.hap.Characteristic.SerialNumber,
-        information.serialNumber,
-      );
-    }
-    if (information.firmwareRevision) {
-      service.useCharacteristic(
-        this.platform.api.hap.Characteristic.FirmwareRevision,
-        information.firmwareRevision,
-      );
-    }
-    if (information.hardwareRevision) {
-      service.useCharacteristic(
-        this.platform.api.hap.Characteristic.HardwareRevision,
-        information.hardwareRevision,
-      );
-    }
+    // const service = this.useService(
+    //   this.platform.Service.AccessoryInformation,
+    //   this.params.name,
+    // );
+    // // Updates the information characteristics
+    // if (information.manufacturer) {
+    //   service.useCharacteristic(
+    //     this.platform.api.hap.Characteristic.Manufacturer,
+    //     information.manufacturer,
+    //   );
+    // }
+    // if (information.model) {
+    //   service.useCharacteristic(
+    //     this.platform.api.hap.Characteristic.Model,
+    //     information.model,
+    //   );
+    // }
+    // if (information.serialNumber) {
+    //   service.useCharacteristic(
+    //     this.platform.api.hap.Characteristic.SerialNumber,
+    //     information.serialNumber,
+    //   );
+    // }
+    // if (information.firmwareRevision) {
+    //   service.useCharacteristic(
+    //     this.platform.api.hap.Characteristic.FirmwareRevision,
+    //     information.firmwareRevision,
+    //   );
+    // }
+    // if (information.hardwareRevision) {
+    //   service.useCharacteristic(
+    //     this.platform.api.hap.Characteristic.HardwareRevision,
+    //     information.hardwareRevision,
+    //   );
+    // }
   }
 
   /**
