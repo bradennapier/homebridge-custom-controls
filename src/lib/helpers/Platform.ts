8861f3c7ce549e82954adbab54f64cc1ebe1160f
Braden Napier <bradynapier+github@gmail.com>
2022-08-16 01:31:40 -0400

    feat: try logging change

--- src/lib/helpers/Platform.ts
+++ src/lib/helpers/Platform.ts
@@ -70,7 +70,7 @@ export class Platform implements DynamicPlatformPlugin {
    * It should be used to setup event handlers for characteristics and update respective values.
    */
   configureAccessory(accessory: PlatformAccessory<AccessoryCreationParams>) {
-    this.log.info('Loading accessory from cache:', accessory.displayName);
+    this.log.info('Loading accessory from cache:', accessory);
 
     // add the restored accessory to the accessories cache so we can track if it has already been registered
     this.accessories.set(accessory.UUID, accessory);
